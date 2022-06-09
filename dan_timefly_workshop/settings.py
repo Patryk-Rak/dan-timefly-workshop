dd86ff308a74ec43b64f5df97b23344bed2ad934
Dan <dan.timefly@gmail.com>
2022-06-10 01:56:15 +0200

    first homepage style and changes in static files

--- dan_timefly_workshop/settings.py
+++ dan_timefly_workshop/settings.py
@@ -108,8 +108,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
 ]
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')\
 
