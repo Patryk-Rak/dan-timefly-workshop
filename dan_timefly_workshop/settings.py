403cd18cf55487a25e69d27b909581e0c97d86f5
Dan <dan.timefly@gmail.com>
2022-06-25 17:54:57 +0200

    Automatic configuration with django-heroku

--- dan_timefly_workshop/settings.py
+++ dan_timefly_workshop/settings.py
@@ -120,3 +120,7 @@ BASE_DIR = "http://127.0.0.1:8000"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
