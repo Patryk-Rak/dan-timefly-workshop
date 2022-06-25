36e590520afcf93e5cff6cd6f812b253f2ec07d8
Dan <dan.timefly@gmail.com>
2022-06-25 19:33:42 +0200

    Relocated django_heroku.settings

--- dan_timefly_workshop/settings.py
+++ dan_timefly_workshop/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['dan-timefly-workshop.herokuapp.com']
 
 
 INSTALLED_APPS = [
